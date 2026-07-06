f30ed4ef0a49539be6c01e7501a3ae6afd5e32c8
h4emin-web <h4emin-web@users.noreply.github.com>
2026-07-06 15:28:11 +0900

    Add Supabase anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.HAEMIN_SUPABASE = {
   url: "https://mecdxdhkiseggcnzlaso.supabase.co",
-  anonKey: "YOUR_SUPABASE_ANON_KEY"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lY2R4ZGhraXNlZ2djbnpsYXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMDQ0MTIsImV4cCI6MjA5ODg4MDQxMn0.vjO3dtAo_Kk5TmaxdTQSk0mXRsIBUMelJb4xOPkubqQ"
 };
